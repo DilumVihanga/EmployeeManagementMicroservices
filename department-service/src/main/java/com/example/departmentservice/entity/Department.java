42d8a6d97f7ca5932366db0cc5df110a048230d6
DilumVihanga <vihangadilum6@gmail.com>
2023-10-26 12:07:17 +0530

    microservices and databases are connected

---
+++ department-service/src/main/java/com/example/departmentservice/entity/Department.java
@@ -0,0 +1,25 @@
+package com.example.departmentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+
+}
